e78eec4dfb33977f98fa1e781eec560c7fd3f3b1
Sandy Ynag <yang21@uw.edu>
2019-03-05 20:08:40 -0800

    add

--- server.R
+++ server.R
@@ -1,1 +1,2 @@
-crime <- read.csv("Seattle_Crime_Stats_by_Police_Precinct_2008-Present.csv")
+crime <- read.csv("data/Seattle_Crime_Stats_by_Police_Precinct_2008-Present.csv")
+gun_shoot <- read.csv("data/SPD_Officer_Involved_Shooting__OIS__Data.csv")
